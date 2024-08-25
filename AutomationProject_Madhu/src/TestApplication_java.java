d89fa9a34822d376610498d4b3e76656b7fb7222
rakshan2652 <rakshan2652@gmail.com>
2024-08-25 13:05:55 +0530

    Amal updated to source

--- AutomationProject_Madhu/src/TestApplication_java.java
+++ AutomationProject_Madhu/src/TestApplication_java.java
@@ -5,6 +5,7 @@ public class TestApplication_java {
 		System.out.println("New Project Java verfication");
 		System.out.println("Eclispe>>GIT ");
 		System.out.println("E2E Integration");
+		System.out.println("Amal Developer");
 	}
 
 }
