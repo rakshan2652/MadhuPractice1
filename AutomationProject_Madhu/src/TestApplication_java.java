849bdd6942a999e136d146928fa5829c2fd470a7
rakshan2652 <rakshan2652@gmail.com>
2024-08-25 12:31:14 +0530

    Updated with new content

--- AutomationProject_Madhu/src/TestApplication_java.java
+++ AutomationProject_Madhu/src/TestApplication_java.java
@@ -3,7 +3,8 @@ public class TestApplication_java {
 
 	public static void main(String[] args) {
 		System.out.println("New Project Java verfication");
-
+		System.out.println("Eclispe>>GIT ");
+		System.out.println("E2E Integration");
 	}
 
 }
